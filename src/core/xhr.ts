962911617c6cc5a98548d0f787220e572ca223a7
zhangxiaodong <774793337@qq.com>
2021-01-27 16:37:47 +0800

    add response promise mode

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -1,21 +1,46 @@
-import { AxiosRequestConfig } from '../types/index'
+import { AxiosRequestConfig, AxiosResponse, AxiosPromise } from '../types/index'
+import { parseHeaders } from '../helpers/headers'
 
-export default function xhr(config: AxiosRequestConfig): void {
-  const { url, method = 'get', data = null, headers = {} } = config
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise(resolve => {
+    const { url, method = 'get', data = null, headers = {}, responseType } = config
 
-  const request = new XMLHttpRequest()
+    const request = new XMLHttpRequest()
 
-  // method必须转换为全大写，url不能为空，async开启异步
-  request.open(method.toUpperCase(), url!, true)
-
-  // 设置请求头
-  Object.keys(headers).forEach((key) => {
-    if (data === null && key.toLowerCase() === 'content-type') {
-      delete headers[key]
-    } else {
-      request.setRequestHeader(key, headers[key])
+    if (responseType) {
+      request.responseType = responseType
     }
-  })
 
-  request.send(data)
+    // method必须转换为全大写，url不能为空，async开启异步
+    request.open(method.toUpperCase(), url!, true)
+
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
+
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+      const responseData = responseType && responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      resolve(response)
+    }
+
+    // 设置请求头
+    Object.keys(headers).forEach((key) => {
+      if (data === null && key.toLowerCase() === 'content-type') {
+        delete headers[key]
+      } else {
+        request.setRequestHeader(key, headers[key])
+      }
+    })
+
+    request.send(data)
+  })
 }
