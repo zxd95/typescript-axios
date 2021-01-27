b2c591424a29fede2f479c0b60bb868ed6782488
zhangxiaodong <774793337@qq.com>
2021-01-27 16:36:39 +0800

    add response mode

--- src/types/index.ts
+++ src/types/index.ts
@@ -21,4 +21,17 @@ export interface AxiosRequestConfig {
   data?: any
   params?: any
   headers?: any
+  responseType?: XMLHttpRequestResponseType // '' | arraybuffer | blob | document | json | text | ms-stream
 }
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+// 继承Promise泛型接口
+export interface AxiosPromise extends Promise<AxiosResponse> { }
