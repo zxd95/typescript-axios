eb54644049b83d08e71e8e3bca6f8c8e8077b261
zhangxiaodong <774793337@qq.com>
2021-02-05 15:01:43 +0800

    add error mode

---
+++ src/axios.ts
@@ -0,0 +1,44 @@
+import { AxiosRequestConfig, AxiosResponse, AxiosPromise } from './types/index'
+import xhr from './core/xhr'
+import { buildURL } from './helpers/url'
+import { transformRequest, transformResponse } from './helpers/data'
+import { processHeaders } from './helpers/headers'
+
+export default function axios(config: AxiosRequestConfig): AxiosPromise {
+  // 处理URL参数
+  processConfig(config)
+  // 实现XMLHttpRequest对象通讯
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
+}
+
+// 处理config参数配置
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
+}
+
+// 处理请求参数params
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url!, params)
+}
+
+// 处理请求参数data
+function transformRequestData(config: AxiosRequestConfig): any {
+  const { data } = config
+  return transformRequest(data)
+}
+
+// 处理请求参数headers参数
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
+}
