2e05b32d50da4af5358e9da35b041118502f521c
zhangxiaodong <774793337@qq.com>
2021-02-05 16:06:06 +0800

    add extend axios request

--- src/types/index.ts
+++ src/types/index.ts
@@ -16,7 +16,7 @@ export type Method =
   | 'PATCH'
 
 export interface AxiosRequestConfig {
-  url: string
+  url?: string
   method?: Method
   data?: any
   params?: any
@@ -35,7 +35,7 @@ export interface AxiosResponse {
 }
 
 // 继承Promise泛型接口
-export interface AxiosPromise extends Promise<AxiosResponse> {}
+export interface AxiosPromise extends Promise<AxiosResponse> { }
 
 export interface AxiosError extends Error {
   isAxiosError: boolean
@@ -43,4 +43,27 @@ export interface AxiosError extends Error {
   code?: string | null
   request?: any
   response?: AxiosResponse
+}
+
+export interface Axios {
+  request(config: AxiosRequestConfig): AxiosPromise
+
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+}
+
+// 混合类型接口
+export interface AxiosInstance extends Axios {
+  (config: AxiosRequestConfig): AxiosPromise
 }
