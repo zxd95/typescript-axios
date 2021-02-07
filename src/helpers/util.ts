f02775ceef2f473b84f921d171d4ac0b6c9c17dd
zhangxiaodong <774793337@qq.com>
2021-02-07 10:33:51 +0800

    add extend axios request

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -8,3 +8,11 @@ export function isDate(val: any): val is Date {
 export function isPlainObject(val: any): val is Object {
   return toString.call(val) === '[object Object]'
 }
+
+// 联合类型
+export function extend<T, U>(to: T, from: U): T & U {
+  for (const key in from) {
+    (to as T & U)[key] = from[key] as any
+  }
+  return to as T & U
+}
