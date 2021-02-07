14b4301b537462921313f9662f070108dca6199f
zhangxiaodong <774793337@qq.com>
2021-02-07 15:19:56 +0800

    modify

--- src/types/index.ts
+++ src/types/index.ts
@@ -66,4 +66,5 @@ export interface Axios {
 // 混合类型接口
 export interface AxiosInstance extends Axios {
   (config: AxiosRequestConfig): AxiosPromise
+  (url: string, config?: AxiosRequestConfig): AxiosPromise
 }
