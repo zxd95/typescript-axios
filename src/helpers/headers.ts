80eff547fb6c326bd0554fcf6395433126d3199c
zhangxiaodong <774793337@qq.com>
2021-02-19 15:57:35 +0800

    新增请求参数合并配置功能

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -1,4 +1,5 @@
-import { isPlainObject } from "./util";
+import { isPlainObject, deepMerge } from './util'
+import { Method } from '../types'
 
 export function processHeaders(headers: any, data: any): any {
   normalizeHeaderName(headers, 'Content-Type')
@@ -41,4 +42,20 @@ export function parseHeaders(headers: string): any {
   })
 
   return parsed
+}
+
+export function flattenHeaders(headers: any, method: Method): any {
+  if (!headers) {
+    return headers
+  }
+
+  headers = deepMerge(headers.common || {}, headers[method] || {}, headers)
+
+  const methodsToDelete = ['delete', 'get', 'head', 'options', 'post', 'put', 'patch', 'common']
+
+  methodsToDelete.forEach(method => {
+    delete headers[method]
+  })
+
+  return headers
 }
