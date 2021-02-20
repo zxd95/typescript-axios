45c80ae5f9ba9eb85efd79423ce35250663ce951
zhangxiaodong <774793337@qq.com>
2021-02-20 10:49:23 +0800

    新增config demo

--- examples/server.js
+++ examples/server.js
@@ -30,6 +30,10 @@ const router = express.Router()
 
 registerSimpleRouter()
 registerBaseRouter()
+registerErrorRouter()
+registerExtendRouter()
+registerInterceptorRouter()
+registerConfigRouter()
 
 app.use(router)
 
@@ -87,5 +91,60 @@ function registerErrorRouter() {
         msg: `hello world`
       })
     }, 3000)
+  })
+}
+
+function registerExtendRouter() {
+  router.get('/extend/get', function(req, res) {
+    res.json({
+      msg: 'hello world'
+    })
+  })
+
+  router.options('/extend/options', function(req, res) {
+    res.end()
+  })
+
+  router.delete('/extend/delete', function(req, res) {
+    res.end()
+  })
+
+  router.head('/extend/head', function(req, res) {
+    res.end()
+  })
+
+  router.post('/extend/post', function(req, res) {
+    res.json(req.body)
+  })
+
+  router.put('/extend/put', function(req, res) {
+    res.json(req.body)
+  })
+
+  router.patch('/extend/patch', function(req, res) {
+    res.json(req.body)
+  })
+
+  router.get('/extend/user', function(req, res) {
+    res.json({
+      code: 0,
+      message: 'ok',
+      result: {
+        name: 'jack',
+        age: 18
+      }
+    })
+  })
+}
+
+function registerInterceptorRouter() {
+  router.get('/interceptor/get', function(req, res) {
+    res.end('hello')
+  })
+}
+
+function registerConfigRouter() {
+  router.post('/config/post', function(req, res) {
+    res.json(req.body)
   })
 }
