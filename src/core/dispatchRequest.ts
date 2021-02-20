8dbe3b9c01ca656baeff2bb85ef53f18a6874f9e
zhangxiaodong <774793337@qq.com>
2021-02-20 10:43:52 +0800

    请求和响应配置化

--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -1,8 +1,8 @@
 import { AxiosRequestConfig, AxiosResponse, AxiosPromise } from '../types/index'
 import xhr from '../core/xhr'
 import { buildURL } from '../helpers/url'
-import { transformRequest, transformResponse } from '../helpers/data'
-import { processHeaders, flattenHeaders } from '../helpers/headers'
+import { flattenHeaders } from '../helpers/headers'
+import transform from './transform'
 
 export default function dispatchAxios(config: AxiosRequestConfig): AxiosPromise {
   // 处理URL参数
@@ -16,8 +16,7 @@ export default function dispatchAxios(config: AxiosRequestConfig): AxiosPromise 
 // 处理config参数配置
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
+  config.data = config.data = transform(config.data, config.headers, config.transformRequest)
   config.headers = flattenHeaders(config.headers, config.method!)
 }
 
@@ -27,19 +26,7 @@ function transformURL(config: AxiosRequestConfig): string {
   return buildURL(url!, params)
 }
 
-// 处理请求参数data
-function transformRequestData(config: AxiosRequestConfig): any {
-  const { data } = config
-  return transformRequest(data)
-}
-
-// 处理请求参数headers参数
-function transformHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
 function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
+  res.data = transform(res.data, res.headers, res.config.transformResponse)
   return res
 }
